181793e09f617d2d02747f7dba5805073e85f1d7
anandureghu <anandukr145@gmail.com>
2021-09-01 23:09:34 +0530

    error correction

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+    $(".about-section").waypoint(direction=>{
+        if(direction==="down"){
+            $("nav").addClass("sticky-nav");
+        }else{
+            $("nav").removeClass("sticky-nav");
+        }
+    });
+
+    $(".about-section").waypoint(direction=>{
+        $(".about-section").addClass("animate__animated animate__fadeInUp");
+    },{
+        offset:"85%"
+    });
+
+    $(".services-section").waypoint(direction=>{
+        $(".services-section").addClass("animate__animated animate__fadeInUp");
+    },{
+        offset:"85%"
+    });
+
+    $(".contact-section").waypoint(direction=>{
+        $(".contact-section").addClass("animate__animated animate__fadeInUp");
+    },{
+        offset:"85%"
+    });
+});
